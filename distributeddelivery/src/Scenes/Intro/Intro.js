a8a403504415f3094ff3ddbffc4ea8d0442aec38
Cameron Raymond <15cjkr@queensu.ca>
2019-02-02 02:42:50 -0500

    Lets gooo

---
+++ distributeddelivery/src/Scenes/Intro/Intro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+   
+  };
+
+  render() {
+    return (
+      <div style={{height: '100vh', width: '100vw', backgroundColor:'#F8F8F8'}}>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
--- distributeddelivery/src/index.js
+++ distributeddelivery/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Map from './Scenes/Map'
+import Intro from './Scenes/Intro/Intro'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Map />, document.getElementById('root'));
+ReactDOM.render(<Intro />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
